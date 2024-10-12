7ac999fea4fa789d61e380c2f99e02bc244439db
AvianHan <hanshuangyue310@gmail.com>
2024-10-12 16:31:32 +0800

    update client.java

--- client_java/UDPClient.java
+++ client_java/UDPClient.java
@@ -5,12 +5,22 @@ public class UDPClient {
     private static final server_address = "";
     private static final port_number = "";
 
-    public static void main (String server_address, String port_number) {
+    public static void main(String server_address, String port_number) {
         DatagramSocket aSocket = null;
         try {
             aSocket = new DatagramSocket();
             byte[] message = message.getBytes();
-            
+            while (true) {
+                DatagramPacket request = new DatagramPacket(message, message.length);
+                aSocket.receive(request);
+                DatagramPacket reply =new DatagramPacket(
+                    request.getData(), request.getLength(),
+                    request.getAddress(), request.getPort()
+                );
+                aSocket.send(reply);
+            }
         }
+        if (aSocket != null)
+            aSocket.close();
     }
 }
