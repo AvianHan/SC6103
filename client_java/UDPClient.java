fce970f4b2431c02b3494e51927c2d3e6bf31234
AvianHan <hanshuangyue310@gmail.com>
2024-10-12 15:52:27 +0800

    add client_java

---
+++ client_java/UDPClient.java
@@ -0,0 +1,11 @@
+import java.net.*;
+import java.io.*;
+
+public class UDPClient {
+    private static final server_address = "";
+    private static final port_number = "";
+
+    public static void main (String server_address, String port_number) {
+        DatagramSocket aSocket = null;
+    }
+}
