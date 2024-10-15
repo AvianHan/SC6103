38ea951367adf368f68cc12d275a7827365ceed8
lunar-huang <lunarhuang@outlook.com>
2024-10-16 01:37:08 +0800

    Update callback_handler.c

--- src/server_c/callback_handler.c
+++ src/server_c/callback_handler.c
@@ -52,23 +52,110 @@ void register_flight_monitor(int sockfd, struct sockaddr_in *client_addr, int fl
 }
 
 // 航班监控线程函数
-void *monitor_flights(void *arg)
+// void *monitor_flights(void *arg)
+// {
+
+//     struct client_data *data = (struct client_data *)arg;
+//     char reply[BUFFER_SIZE];
+
+//     // 使用传入的数据库连接
+//     // MYSQL *conn = data->conn;  // 现在可以在这个线程中使用 conn
+
+//     // int sockfd = *(int*)arg;
+//     MYSQL *conn = connect_db(); // 连接数据库
+
+//     while (1)
+//     {
+//         pthread_mutex_lock(&flight_mutex);
+
+//         // 遍历每个客户端监控的航班
+//         for (int i = 0; i < client_monitor_count; i++)
+//         {
+//             int flight_id = client_monitors[i].flight_id;
+
+//             // 查询当前航班的座位可用性
+//             char query[256];
+//             snprintf(query, sizeof(query), "SELECT seat_availability FROM flights WHERE flight_id = %d", flight_id);
+//             printf("LOOP I:now we have %d client_monitors\n", client_monitor_count);
+//             if (mysql_query(conn, query))
+//             {
+//                 fprintf(stderr, "SELECT error: %s\n", mysql_error(conn));
+//                 continue;
+//             }
+
+//             MYSQL_RES *res = mysql_store_result(conn);
+//             if (res == NULL)
+//             {
+//                 fprintf(stderr, "mysql_store_result() failed: %s\n", mysql_error(conn));
+//                 continue;
+//             }
+
+//             MYSQL_ROW row = mysql_fetch_row(res);
+//             if (row)
+//             {
+//                 int current_seat_availability = atoi(row[0]);
+//                 printf("LOOP J: now current_seat_availability is %d\n", current_seat_availability);
+
+//                 // 比较当前座位可用数量是否变化
+//                 for (int j = 0; j < client_monitor_count; j++)
+//                 {
+//                     if (client_monitors[j].flight_id == flight_id)
+//                     {
+//                         if (client_monitors[j].seat_availability != current_seat_availability)
+//                         {
+//                             client_monitors[j].seat_availability = current_seat_availability; // 更新状态
+//                             printf("seats avail changed\n");
+//                             // 通知注册监控该航班的客户端
+//                             char response[BUFFER_SIZE];
+//                             snprintf(response, sizeof(response),
+//                                      "Flight %d seat availability updated to %d\n",
+//                                      flight_id, current_seat_availability);
+
+//                             // 将更新发送到注册的客户端
+//                             // sendto(data->sockfd, response, strlen(response), 0,(struct sockaddr *)data->client_addr,sizeof(data->client_addr));
+//                             ssize_t sent_len = sendto(data->sockfd, response, strlen(response), 0,
+//                                                       (struct sockaddr *)&data->client_addr,
+//                                                       sizeof(data->client_addr));
+
+//                             // 判断 sendto 是否成功
+//                             if (sent_len == -1)
+//                             {
+//                                 // 输出错误信息
+//                                 perror("Failed to send data with sendto");
+//                             }
+//                             else
+//                             {
+//                                 // 输出成功发送的字节数
+//                                 printf("Successfully sent %ld bytes to the client\n", sent_len);
+//                             }
+//                             // sendto(sockfd, response, strlen(response), 0, (struct sockaddr *)client_addr, sizeof(*client_addr));
+//                         }
+//                     }
+//                 }
+//             }
+
+//             mysql_free_result(res);
+//         }
+
+//         pthread_mutex_unlock(&flight_mutex);
+//         // printf("check in flight seat availability\n");
+//         sleep(5); // 每5秒查询一次数据库
+//     }
+
+//     close_db(conn); // 关闭数据库连接
+//     return NULL;
+// }
+
+void* monitor_flights(void* arg)
 {
-
     struct client_data *data = (struct client_data *)arg;
-    char reply[BUFFER_SIZE];
-
-    // 使用传入的数据库连接
-    // MYSQL *conn = data->conn;  // 现在可以在这个线程中使用 conn
-
-    // int sockfd = *(int*)arg;
     MYSQL *conn = connect_db(); // 连接数据库
 
     while (1)
     {
-        pthread_mutex_lock(&flight_mutex);
+        pthread_mutex_lock(&flight_mutex); // 加锁，保证线程安全
 
-        // 遍历每个客户端监控的航班
+        // 遍历所有监控的航班，确保只查询一次数据库
         for (int i = 0; i < client_monitor_count; i++)
         {
             int flight_id = client_monitors[i].flight_id;
@@ -76,7 +163,7 @@ void *monitor_flights(void *arg)
             // 查询当前航班的座位可用性
             char query[256];
             snprintf(query, sizeof(query), "SELECT seat_availability FROM flights WHERE flight_id = %d", flight_id);
-            printf("LOOP I:exec SELECT seat_availability FROM flights WHERE flight_id = %d\n", flight_id);
+
             if (mysql_query(conn, query))
             {
                 fprintf(stderr, "SELECT error: %s\n", mysql_error(conn));
@@ -94,54 +181,51 @@ void *monitor_flights(void *arg)
             if (row)
             {
                 int current_seat_availability = atoi(row[0]);
-                printf("LOOP J: now current_seat_availability is %d\n", current_seat_availability);
 
-                // 比较当前座位可用数量是否变化
-                for (int j = 0; j < client_monitor_count; j++)
+                // 比较当前座位可用数量是否变化，只有变化时通知客户端
+                if (client_monitors[i].seat_availability != current_seat_availability)
                 {
-                    if (client_monitors[j].flight_id == flight_id)
+                    client_monitors[i].seat_availability = current_seat_availability; // 更新座位信息
+                    printf("Seats availability changed for flight %d, notifying clients\n", flight_id);
+
+                    // 发送通知给所有监控该航班的客户端
+                    for (int j = 0; j < client_monitor_count; j++)
                     {
-                        if (client_monitors[j].seat_availability != current_seat_availability)
+                        if (client_monitors[j].flight_id == flight_id)
                         {
-                            client_monitors[j].seat_availability = current_seat_availability; // 更新状态
-                            printf("seats avail changed\n");
-                            // 通知注册监控该航班的客户端
+                            // 通知客户端
                             char response[BUFFER_SIZE];
                             snprintf(response, sizeof(response),
                                      "Flight %d seat availability updated to %d\n",
                                      flight_id, current_seat_availability);
 
-                            // 将更新发送到注册的客户端
-                            // sendto(data->sockfd, response, strlen(response), 0,(struct sockaddr *)data->client_addr,sizeof(data->client_addr));
+                            // 发送消息给客户端
                             ssize_t sent_len = sendto(data->sockfd, response, strlen(response), 0,
-                                                      (struct sockaddr *)&data->client_addr,
-                                                      sizeof(data->client_addr));
+                                                      (struct sockaddr *)&client_monitors[j].client_addr,
+                                                      sizeof(client_monitors[j].client_addr));
 
-                            // 判断 sendto 是否成功
+                            // 判断发送是否成功
                             if (sent_len == -1)
                             {
-                                // 输出错误信息
                                 perror("Failed to send data with sendto");
                             }
                             else
                             {
-                                // 输出成功发送的字节数
                                 printf("Successfully sent %ld bytes to the client\n", sent_len);
                             }
-                            // sendto(sockfd, response, strlen(response), 0, (struct sockaddr *)client_addr, sizeof(*client_addr));
                         }
                     }
                 }
             }
-
-            mysql_free_result(res);
+            mysql_free_result(res); // 释放结果集
         }
 
-        pthread_mutex_unlock(&flight_mutex);
-        // printf("check in flight seat availability\n");
-        sleep(5); // 每5秒查询一次数据库
+        pthread_mutex_unlock(&flight_mutex); // 解锁
+
+        sleep(5); // 每5秒检查一次
     }
 
     close_db(conn); // 关闭数据库连接
     return NULL;
 }
+
